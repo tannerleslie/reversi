2508cc9de71eb864318f603b71ae4156dc1ef406
Tanner Leslie <tleslie@westmont.edu>
2020-03-30 22:30:43 -0700

    Expanded the lobby

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,10 @@ if('undefined' == typeof username || !username){
   username = 'anonymous_' + Math.random();
 }
 
-var chat_room = 'One_Room';
+var chat_room = getURLParameters('game_id');
+if('undefined' == typeof chat_room || !chat_room){
+  chat_room = 'lobby';
+}
 
 /* Connect ot the socket server*/
 
@@ -30,7 +33,68 @@ socket.on('join_room_response', function(payload){
     alert(payload.message);
     return;
   }
-  $('#messages').append('<p>New user joined the room: '+payload.username+'</p>');
+  if(payload.socket_id == socket.id){
+    return;
+  }
+
+  var dom_elements = $('.socket_'+payload.socket_id);
+  if(dom_elements.length == 0){
+    var nodeA = $('<div></div>');
+    nodeA.addClass('socket_'+payload.socket_id);
+    var nodeB = $('<div></div>');
+    nodeB.addClass('socket_'+payload.socket_id);
+    var nodeC = $('<div></div>');
+    nodeC.addClass('socket_'+payload.socket_id);
+
+    nodeA.addClass('w-100');
+
+    nodeB.addClass('col-9 text-right');
+    nodeB.append('<h4>'+payload.username+'</h4>');
+
+    nodeC.addClass('col-3 text-left');
+    var buttonC = makeInviteButton();
+    nodeC.append(buttonC);
+
+    nodeA.hide();
+    nodeB.hide();
+    nodeC.hide();
+    $('#players').append(nodeA,nodeB,nodeC);
+    nodeA.slideDown(1000);
+    nodeB.slideDown(1000);
+    nodeC.slideDown(1000);
+  }
+  else{
+    var buttonC = makeInviteButton();
+    $('.socket_'+payload.socket_id+' button').replaceWith(buttonC);
+    dom_elements.slideDown(1000);
+  }
+
+  var newHTML = '<p>'+payload.username+' just entered the lobby</p>';
+  var newNode = $(newHTML);
+  newNode.hide();
+  $('#messages').append(newNode);
+  newNode.slideDown(1000);
+});
+
+socket.on('player_disconnected', function(payload){
+  if(payload.result == 'fail'){
+    alert(payload.message);
+    return;
+  }
+  if(payload.socket_id == socket.id){
+    return;
+  }
+
+  var dom_elements = $('.socket_'+payload.socket_id);
+  if(dom_elements.length != 0){
+    dom_elements.slideUp(1000);
+  }
+
+  var newHTML = '<p>'+payload.username+' has left the lobby</p>';
+  var newNode = $(newHTML);
+  newNode.hide();
+  $('#messages').append(newNode);
+  newNode.slideDown(1000);
 });
 
 socket.on('send_message_response', function(payload){
@@ -50,6 +114,12 @@ function send_message(){
 
   console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
   socket.emit('send_message',payload);
+}
+
+function makeInviteButton(){
+  var newHTML = "<button type='button' class='btn btn-outline-primary'>Invite</button>";
+  var newNode = $(newHTML);
+  return(newNode);
 }
 
 $(function(){
